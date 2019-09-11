0dd8ddef41339f5834d80faa4349f21e8f0d65f1
zxalif <sajeeb162537@gmail.com>
2019-09-11 22:44:39 +0600

    Created game models

--- game/models.py
+++ game/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30,
+                            unique=True,
+                            verbose_name='Category Name')
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=120,
+                            unique=True,
+                            verbose_name='Game Name')
+    image = models.ImageField()
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
