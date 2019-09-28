ee6c5ac3112bf0d54e9db06bc1abdc4002e6ca52
zxalif <sajeeb162537@gmail.com>
2019-09-28 13:59:11 +0600

    Added thread into model

--- game/admin.py
+++ game/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 from .models import (
     Category,
-    Game
+    Game,
+    Thread,
+    ThreadImage
 )
 
 admin.site.register(Category)
 admin.site.register(Game)
+admin.site.register(Thread)
+admin.site.register(ThreadImage)
--- game/models.py
+++ game/models.py
@@ -2,21 +2,62 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30,
-                            unique=True,
-                            verbose_name='Category Name')
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+        verbose_name='Category Name'
+    )
 
     def __str__(self):
         return self.name
 
 
 class Game(models.Model):
-    name = models.CharField(max_length=120,
-                            unique=True,
-                            verbose_name='Game Name')
+    name = models.CharField(
+        max_length=120,
+        unique=True,
+        verbose_name='Game Name'
+    )
     image = models.ImageField()
-    category = models.ForeignKey(Category,
-                                 on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
+
+
+class Thread(models.Model):
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE,
+    )
+    alias_name = models.CharField(
+        max_length=120
+    )
+    email = models.EmailField(
+        max_length=70,
+        db_index=True,
+        null=True,
+    )
+    anonymous = models.BooleanField(
+        default=False,
+        verbose_name='Hide details'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.alias_name
+
+class ThreadImage(models.Model):
+    thread = models.ForeignKey(
+        Thread,
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.image.name
